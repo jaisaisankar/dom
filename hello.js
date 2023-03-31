30bce5ec0811cd8ed6aa68d412c6857add85748f
jaisaisankar <129482380+jaisaisankar@users.noreply.github.com>
2023-03-31 23:56:51 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,17 @@
+var itemlist=document.querySelector('#items');
+parentElement
+lastelementchild
+lastElement
+createchild
+firstelementchild
+firstchild
+nextsibling
+nextelementsibling
+previoussibling
+previouselementsibling
+createelement
+setAttribute
+createtesxtnode
+appendchild
+children
+itemlist.parentElement.style.backgroundColor='red';
